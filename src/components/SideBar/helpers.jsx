5456283b485f701dbb16772ee3bf22de2a10104a
ronan sibunga <ronan.sibunga@veritaspay.com>
2023-08-02 13:12:30 +0800

    added files

---
+++ src/components/SideBar/helpers.jsx
@@ -0,0 +1,44 @@
+import House from "../../assets/sidebaricons/house.svg";
+import Billing from "../../assets/sidebaricons/billing.svg"
+import Chart from "../../assets/sidebaricons/chart.svg"
+import Wrench from "../../assets/sidebaricons/wrench.svg"
+
+export const icons = [
+  {
+    title: "Dashboard",
+    icon: (House ),
+  },
+  {
+    title: "Tables",
+    icon: (Chart),
+  },
+  {
+    title: "Billing",
+    icon: (Billing),
+
+  },
+  {
+    title: "RTL",
+    icon: (Wrench
+   
+    ),
+  },
+];
+
+
+export const iconstwo = [
+  {
+    title: "Profile",
+    icon: (Billing ),
+  },
+  {
+    title: "Sign In",
+    icon: (Billing),
+  },
+  {
+    title: "Sign Up",
+    icon: (Billing),
+
+  },
+
+];
---
+++ src/components/SideBarIcons/SideBarIcons.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './SibeBarIcons.css';
+
+
+
+
+
+
+const SideBarIcons = ({ icons, title, isActive, setActiveIcon }) => {
+
+    return (
+      <div
+        className={isActive ? 'side-bar-icon-active' : 'side-bar-icon'}
+        onClick={() => setActiveIcon(isActive ? null : title)}
+      >
+        <div className={isActive ? 'icon-wrapper-active' : 'icon-wrapper'}>
+          <div className='icon-box'>
+            <img src={icons} alt='icon'></img>
+      
+            
+          </div>
+        </div>
+        <div className='icon-title'>
+          <span>{title}</span>
+        </div>
+  
+      </div>
+    );
+  };
+  
+  export default SideBarIcons;
